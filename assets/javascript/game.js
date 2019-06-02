0b7229aa38bc9bfa4d0721919cff923650c22eaa
Dave Hawkins <dhawkins01@gmail.com>
2019-06-01 21:06:00 -0400

    added some javascript with console.log to test initial values

---
+++ assets/javascript/game.js
@@ -0,0 +1,18 @@
+/// Global Variables
+
+// set the initial values
+var targetNumber = Math.floor(Math.random() * 102) + 19;
+var blueGem = Math.floor(Math.random() * 12) + 1;
+var orangeGem = Math.floor(Math.random() * 12) + 1;
+var redGem = Math.floor(Math.random() * 12) + 1;
+var greenGem = Math.floor(Math.random() * 12) + 1;
+var userScore = 0;
+
+
+
+// logging out for testing purposes
+console.log("target number: " + targetNumber);
+console.log("blueGem: " + blueGem);
+console.log("orangeGem: " + orangeGem);
+console.log("redGem: " + redGem);
+console.log("greenGem: " + greenGem);
