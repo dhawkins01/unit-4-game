707d5f2a6abf48f66d22763b6612a1fbb1b7e7fa
Dave Hawkins <dhawkins01@gmail.com>
2019-06-01 21:30:21 -0400

    html now displays the user and target score

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,6 +7,8 @@ var orangeGem = Math.floor(Math.random() * 12) + 1;
 var redGem = Math.floor(Math.random() * 12) + 1;
 var greenGem = Math.floor(Math.random() * 12) + 1;
 var userScore = 0;
+var win = 0;
+var loses = 0;
 
 
 
@@ -16,3 +18,19 @@ console.log("blueGem: " + blueGem);
 console.log("orangeGem: " + orangeGem);
 console.log("redGem: " + redGem);
 console.log("greenGem: " + greenGem);
+
+// This function resets the values of the gems and the target number when the user wins or loses
+function newGame() {
+    targetNumber = Math.floor(Math.random() * 102) + 19;
+    blueGem = Math.floor(Math.random() * 12) + 1;
+    orangeGem = Math.floor(Math.random() * 12) + 1;
+    redGem = Math.floor(Math.random() * 12) + 1;
+    greenGem = Math.floor(Math.random() * 12) + 1;
+    userScore = 0;
+    $("#target-score").text(targetNumber);
+    $("#your-score").text(userScore);
+}
+
+// Main Game code
+$("#target-score").text(targetNumber);
+$("#your-score").text(userScore);
